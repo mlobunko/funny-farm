e836b9f9c2bb0e38f2a202eee334f1bfb04b265b
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-06-03 14:05:48 +0300

    reduced size of media

--- src/logic/sound.js
+++ src/logic/sound.js
@@ -1,11 +1,11 @@
 import { Howl } from "howler";
-import cat from "../sound/cat.wav";
+import cat from "../sound/cat.mp3";
 import cow from "../sound/cow.mp3";
-import dog from "../sound/dog.wav";
+import dog from "../sound/dog.mp3";
 import duck from "../sound/duck.mp3";
 import music from "../sound/music.mp3";
-import rooster from "../sound/rooster.wav";
-import sheep from "../sound/sheep.wav";
+import rooster from "../sound/rooster.mp3";
+import sheep from "../sound/sheep.mp3";
 
 const catSound = new Howl({
   src: [cat]
