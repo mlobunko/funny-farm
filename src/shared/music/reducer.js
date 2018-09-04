f13154e8e4ca677aaa8c2d4f7d1974f9b60ef62d
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-09-04 12:21:11 +0300

    change initialState of sound to false

--- src/shared/music/reducer.js
+++ src/shared/music/reducer.js
@@ -1,6 +1,6 @@
 import * as t from './actionTypes';
 
-export const initialState = true;
+export const initialState = false;
 
 export default (state = initialState, action) => {
   switch (action.type) {
--- src/shared/music/tests/reducer.test.js
+++ src/shared/music/tests/reducer.test.js
@@ -8,9 +8,10 @@ describe('reducer', () => {
 
   it(`sould handle ${t.TOGGLE}`, () => {
     const expectedState = false;
+    const state = true;
     const action = {
       type: t.TOGGLE
     };
-    expect(reducer(undefined, action)).toEqual(expectedState);
+    expect(reducer(state, action)).toEqual(expectedState);
   });
 });
