305a1c999004c360af5afbc859806c351201bfa9
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-09-11 17:43:07 +0300

    change rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,4 +1,5 @@
-import { all, fork } from 'redux-saga/effects';
+import { all, call, spawn } from 'redux-saga/effects';
+
 import clickedAnimal from 'shared/clickedAnimal';
 import music from 'shared/music';
 import cat from 'app/animals/cat';
@@ -9,15 +10,30 @@ import rooster from 'app/animals/rooster';
 import sheep from 'app/animals/sheep';
 
 export default function* rootSaga() {
-  yield all([
-    fork(clickedAnimal.saga.watchSet),
-    fork(music.saga.watchStart),
-    fork(music.saga.watchClick),
-    fork(cat.saga.watchPlaySound),
-    fork(cow.saga.watchPlaySound),
-    fork(dog.saga.watchPlaySound),
-    fork(duck.saga.watchPlaySound),
-    fork(rooster.saga.watchPlaySound),
-    fork(sheep.saga.watchPlaySound)
-  ]);
+  const sagas = [
+    clickedAnimal.saga.watchSet,
+    music.saga.watchStart,
+    music.saga.watchClick,
+    cat.saga.watchPlaySound,
+    cow.saga.watchPlaySound,
+    dog.saga.watchPlaySound,
+    duck.saga.watchPlaySound,
+    rooster.saga.watchPlaySound,
+    sheep.saga.watchPlaySound
+  ];
+
+  yield all(
+    sagas.map(saga =>
+      spawn(function*() {
+        while (true) {
+          try {
+            yield call(saga);
+            break;
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      })
+    )
+  );
 }
